b2b3633b66f1b672052e94ebf59487ff68522af4
HalFrgrd <hal.frig@gmail.com>
2020-06-15 18:55:08 +0100

    sorted changes

--- keyboards/porkchop/config.h
+++ keyboards/porkchop/config.h
@@ -1,9 +1,5 @@
 /*
-<<<<<<< HEAD
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-=======
 Copyright 2017 REPLACE_WITH_YOUR_NAME
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -25,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-<<<<<<< HEAD
 
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
@@ -48,146 +43,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /* key combination for command */
-=======
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    You
-#define PRODUCT         porkchop
-#define DESCRIPTION     A custom keyboard
-
-/* key matrix size */
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 7
-
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
-*/
-#define MATRIX_ROW_PINS { D3, D2, D4, C6, B1, B3, B2, B6 }
-#define MATRIX_COL_PINS { E6, B4, B5, F4, F5, F6, F7 }
-#define UNUSED_PINS
-
-/* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
-#define DIODE_DIRECTION COL2ROW
- 
-// #define BACKLIGHT_PIN B7
-// #define BACKLIGHT_BREATHING
-// #define BACKLIGHT_LEVELS 3
-
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCING_DELAY 5
-
-/* define if matrix has ghost (lacks anti-ghosting diodes) */
-//#define MATRIX_HAS_GHOST
-
-/* number of backlight levels */
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
- * This is userful for the Windows task manager shortcut (ctrl+shift+esc).
- */
-// #define GRAVE_ESC_CTRL_OVERRIDE
-
-/*
- * Force NKRO
- *
- * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
- * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
- * makefile for this to work.)
- *
- * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
- * until the next keyboard reset.
- *
- * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
- * fully operational during normal computer usage.
- *
- * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
- * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
- * bootmagic, NKRO mode will always be enabled until it is toggled again during a
- * power-up.
- *
- */
-//#define FORCE_NKRO
-
-/*
- * Magic Key Options
- *
- * Magic keys are hotkey commands that allow control over firmware functions of
- * the keyboard. They are best used in combination with the HID Listen program,
- * found here: https://www.pjrc.com/teensy/hid_listen.html
- *
- * The options below allow the magic key functionality to be changed. This is
- * useful if your keyboard/keypad is missing keys and you want magic key support.
- *
- */
-
-/* key combination for magic key command */
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 #define IS_COMMAND() ( \
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
 
-<<<<<<< HEAD
-=======
-/* control how magic key switches layers */
-//#define MAGIC_KEY_SWITCH_LAYER_WITH_FKEYS  true
-//#define MAGIC_KEY_SWITCH_LAYER_WITH_NKEYS  true
-//#define MAGIC_KEY_SWITCH_LAYER_WITH_CUSTOM false
-
-/* override magic key keymap */
-//#define MAGIC_KEY_SWITCH_LAYER_WITH_FKEYS
-//#define MAGIC_KEY_SWITCH_LAYER_WITH_NKEYS
-//#define MAGIC_KEY_SWITCH_LAYER_WITH_CUSTOM
-//#define MAGIC_KEY_HELP1          H
-//#define MAGIC_KEY_HELP2          SLASH
-//#define MAGIC_KEY_DEBUG          D
-//#define MAGIC_KEY_DEBUG_MATRIX   X
-//#define MAGIC_KEY_DEBUG_KBD      K
-//#define MAGIC_KEY_DEBUG_MOUSE    M
-//#define MAGIC_KEY_VERSION        V
-//#define MAGIC_KEY_STATUS         S
-//#define MAGIC_KEY_CONSOLE        C
-//#define MAGIC_KEY_LAYER0_ALT1    ESC
-//#define MAGIC_KEY_LAYER0_ALT2    GRAVE
-//#define MAGIC_KEY_LAYER0         0
-//#define MAGIC_KEY_LAYER1         1
-//#define MAGIC_KEY_LAYER2         2
-//#define MAGIC_KEY_LAYER3         3
-//#define MAGIC_KEY_LAYER4         4
-//#define MAGIC_KEY_LAYER5         5
-//#define MAGIC_KEY_LAYER6         6
-//#define MAGIC_KEY_LAYER7         7
-//#define MAGIC_KEY_LAYER8         8
-//#define MAGIC_KEY_LAYER9         9
-//#define MAGIC_KEY_BOOTLOADER     PAUSE
-//#define MAGIC_KEY_LOCK           CAPS
-//#define MAGIC_KEY_EEPROM         E
-//#define MAGIC_KEY_NKRO           N
-//#define MAGIC_KEY_SLEEP_LED      Z
-
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
  */
 
-<<<<<<< HEAD
 #define PREVENT_STUCK_MODIFIERS
 
-=======
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 /* disable debug print */
 //#define NO_DEBUG
 
@@ -201,7 +67,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
-<<<<<<< HEAD
 //UART settings for communication with the RF microcontroller
 #define SERIAL_UART_BAUD 9600
 #define SERIAL_UART_DATA UDR1
@@ -218,29 +83,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     	/* 8-bit data */ \
     	UCSR1C = _BV(UCSZ11) | _BV(UCSZ10); \
   	} while(0)
-=======
-/*
- * MIDI options
- */
-
-/* Prevent use of disabled MIDI features in the keymap */
-//#define MIDI_ENABLE_STRICT 1
-
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
-//#define MIDI_BASIC
-
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-//#define MIDI_ADVANCED
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 1
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 
 #endif
--- keyboards/porkchop/keymaps/default/keymap.c
+++ keyboards/porkchop/keymaps/default/keymap.c
@@ -1,4 +1,3 @@
-  <<<<<<< HEAD
 #include "mitosis.h"
 #include "print.h"
 #include <print.h>
@@ -25,29 +24,21 @@ enum {
 
 
 enum custom_keycodes {
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
   QWERTY = 0,
   NAVIG,
   NUMBERS,
   FUNC,
   SYMB,
-<<<<<<< HEAD
   BLANCK,
 };
 
 
-=======
-  BLANCK
-};
-
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 enum {
    DB_SQ_BRACK = 10,
    DB_PR_BRACK,
    DB_CR_BRACK,
    DB_COLON,
    DB_QUOT,
-<<<<<<< HEAD
    DB_UNDS,
 
 
@@ -57,16 +48,11 @@ enum {
    ALT_TAB = SAFE_RANGE,
    SFT_ALT_TAB,
    MNAVIG
-=======
-   DB_UNDS
-   
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 };
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-<<<<<<< HEAD
 // Defines for task manager and such
 #define CALTDEL LCTL(LALT(KC_DEL))
 #define TSKMGR LCTL(LSFT(KC_ESC))
@@ -77,16 +63,11 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
-=======
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 /* Qwerty
  *
  * ,----------------------------------.           ,----------------------------------.
  * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
  * |------+------+------+------+------|           |------+------+------+------+------|
-<<<<<<< HEAD
  * |   A  |  S   |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |Shft/Ent|
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |Ctl /Z|  X   |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
@@ -102,28 +83,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    RSFT_T(KC_ENT) , \
   CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
   XXXXXXX, XXXXXXX, KC_LGUI, LT(NUMBERS,KC_SPC), MNAVIG, LT(SYMB,KC_DEL), KC_BSPC , MO(FUNC) , MO(FUNC), XXXXXXX      \
-=======
- * |   A  |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |  Ent |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
- * `----------------------------------'           `----------------------------------'
- *           ,---------------------------.    ,---------------------------.
- *    Tap:   | Esc  | L_GUI|Space | Tab  |    | Enter|BckSpc|      |      |
- *    Hold:  | Ctrl |LShft |Numbs |Navig |    |Symbol| Func |RShft |      |
- *           `---------------------------'    `---------------------------'
- */
-[QWERTY] = LAYOUT( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT , \
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-           MT(KC_LCTL,KC_ESC), MT(MOD_LSFT, KC_LGUI), LT(NUMBERS,KC_SPC), LT(NAVIG,KC_TAB), LT(SYMB,KC_ENT), LT(FUNC,KC_BSPC), OSM(MOD_RSFT), XXXXXXX                 \
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 ),
 
 /* Navigation
 *
 * ,----------------------------------.           ,----------------------------------.
-<<<<<<< HEAD
 * | Esc  |C W   |      |      |CtShfT|           |Ctrl0 | pgUp |  Up  | pgDwn|Alt+F4|
 * |------+------+------+------+------|           |------+------+------+------+------|
 * | CSTb |CTb   |ASTab | AlTb | Tab  |           |ScrUp | Left | Down | Right| Enter|
@@ -186,66 +150,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,     KC_F6  , KC_F7   , KC_F8  , KC_F9   , KC_F10, \
    CALTDEL, TSKMGR , XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_PSCR , XXXXXXX, XXXXXXX , XXXXXXX, \
    XXXXXXX, RESET  , _______, _______, _______,     _______, _______ , _______, _______ , XXXXXXX  \
-=======
-* | Esc  |C W   | C L  |  Esc | C T  |           | CtrY | pgUp |  Up  | pgDwn| End  |
-* |------+------+------+------+------|           |------+------+------+------+------|
-* | CSTb | CTb  |ATb   | Ctrl | Shft |           |ScrlUp| Left | Down | Right| Enter|
-* |------+------+------+------+------|           |------+------+------+------+------|
-* |  Undo|  Cut | Copy |Paste |SelAll|           |ScrDwn|  Home|  Del |BckSpc|CtShfT|
-* `----------------------------------'           `----------------------------------'
-*            ,---------------------------.    ,---------------------------.
-*            |      |      |      |      |    |Ctrl- |Ctrl+ |Ctrl0 |      |
-*            |      |      |      | Navig|    |      |      |      |      |
-*            `---------------------------'    `---------------------------'
-*/
-[NAVIG] = LAYOUT( \
-   KC_ESC , LCTL(KC_W), LCTL(KC_L), KC_ESC, LCTL(KC_T) ,      LCTL(KC_Y), KC_PGUP, KC_UP  , KC_PGDN, KC_END, \
-   LSFT(LCTL(KC_TAB)), LCTL(KC_TAB), LALT(KC_TAB), KC_LCTL, KC_LSHIFT ,      KC_MS_WH_UP, KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT , \
-   LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_A),      KC_MS_WH_DOWN, KC_HOME, KC_DEL , KC_BSPC , LSFT(LCTL(KC_T)), \
-            XXXXXXX, XXXXXXX, XXXXXXX, _______,      LCTL(KC_MINS), LCTL(KC_PLUS), LCTL(KC_0)   , XXXXXXX            \
-),
-
-/* Numbers
- *
- * ,----------------------------------.           ,----------------------------------.
- * |      |      |      |      |      |           |   +  |   7  |   8  |   9  |  -   |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |      |  !   |      |      | DEL  |           | BS   |   4  |   5  |   6  |ENTER |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  (   |   )  |  %   |   <  |  >   |           |   *  |   1  |  2   |  3   |  /   |
- * `----------------------------------'           `----------------------------------'
- *            ,---------------------------.    ,---------------------------.
- *            |      |      |      |      |    |  ^   |  =   |   0  |  .   |
- *            |      |      | Numbs|      |    |      |      |      |      |
- *            `---------------------------'    `---------------------------'
- */
-[NUMBERS] = LAYOUT( \
-   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_PLUS, KC_7   , KC_8   , KC_9   , KC_MINS, \
-   XXXXXXX, KC_EXLM, XXXXXXX, XXXXXXX, KC_DEL ,      KC_BSPC, KC_4   , KC_5   , KC_6   , KC_ENT , \
-   KC_LPRN, KC_RPRN, KC_PERC, KC_LABK, KC_RABK,      KC_ASTR, KC_1   , KC_2   , KC_3   , KC_SLSH, \
-            XXXXXXX, XXXXXXX, _______, KC_CIRC,      KC_EQL, KC_0, KC_DOT, XXXXXXX            \
- ),
-
-/* Function
- *
- * ,----------------------------------.           ,----------------------------------.
- * |NLock |      | Caps |      | VolUp|           |      |  F7  |  F8  |  F9  | F10  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Win | Ctrl | Shift|  Alt | Mute |           |  Ins |  F4  |  F5  |  F6  | F11  |
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |      |      |      |ScrSht|VolDWn|           |      |  F1  |  F2  |  F3  | F12  |
- * `----------------------------------'           `----------------------------------'
- *            ,---------------------------.    ,---------------------------.
- *            |      | Reset|      |      |    |      |      |      |      |
- *            |      |      |      |      |    |      |Func  |      |      |
- *            `---------------------------'    `---------------------------'
- */
-[FUNC] = LAYOUT( \
-   KC_LNUM, XXXXXXX, KC_CAPS, XXXXXXX, KC__VOLUP,    XXXXXXX, KC_F7   , KC_F8   , KC_F9   , KC_F10, \
-   KC_LGUI, KC_LCTL, KC_LSFT, KC_LALT, KC_MUTE,      KC_INS , KC_F4   , KC_F5   , KC_F6   , KC_F11 , \
-   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC__VOLDOWN,  XXXXXXX, KC_F1   , KC_F2   , KC_F3   , KC_F12, \
-            XXXXXXX, RESET  , XXXXXXX, XXXXXXX,      XXXXXXX, _______, XXXXXXX, XXXXXXX            \
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 ),
 
 /* Symbol
@@ -253,22 +157,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |      |  @   |  #   |  $   |  %   |           |  ^   |  &   |   *  | +    |      |
  * |------+------+------+------+------|           |------+------+------+------+------|
-<<<<<<< HEAD
  * |  !   |  [   |  (   |  {   |  <   |           |  ¬   |  ;   |   -  |   '  |  :   |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |  \   |  ]   |  )   |  }   |  >   |           |  |   |  `   |   _  |   "  |  ?   |
-=======
- * |  !   |      |      |      |      |           |      |[ DB ]|{ DB }|( DB )|; DB :|
- * |------+------+------+------+------|           |------+------+------+------+------|
- * |      |  \   |  |   |  ~   |      |           |      |' DB "|- DB _|      |      |
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
  * `----------------------------------'           `----------------------------------'
  *            ,---------------------------.    ,---------------------------.
  *            |      |      |      |      |    |      |      |      |      |
  *            |      |      |      |      |    |Symbol|      |      |      |
  *            `---------------------------'    `---------------------------'
  */
-<<<<<<< HEAD
 [SYMB] = KEYMAP( \
    XXXXXXX, KC_AT  , KC_HASH, KC_DLR , KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, XXXXXXX, \
    KC_EXLM, KC_LBRC, KC_LPRN, KC_LCBR, KC_LABK,      KC_TILD, KC_SCLN, KC_MINS, KC_QUOT, KC_COLN, \
@@ -277,15 +174,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 
-=======
-[SYMB] = LAYOUT( \
-   XXXXXXX, KC_AT  , KC_HASH, KC_DLR , KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, XXXXXXX, \
-   KC_EXLM, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, TD(DB_SQ_BRACK), TD(DB_CR_BRACK), TD(DB_PR_BRACK), TD(DB_COLON), \
-   XXXXXXX, KC_BSLS, KC_PIPE, KC_TILD, XXXXXXX,      XXXXXXX, TD(DB_QUOT), TD(DB_UNDS), XXXXXXX, XXXXXXX, \
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      _______, XXXXXXX, XXXXXXX, XXXXXXX            \
-),
-
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 /* Blank
  *
  * ,----------------------------------.           ,----------------------------------.
@@ -300,7 +188,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |    |      |      |      |      |
  *            `---------------------------'    `---------------------------'
  */
-<<<<<<< HEAD
 [BLANCK] = KEYMAP( \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
@@ -309,48 +196,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 
 
-=======
-[BLANCK] = LAYOUT( \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX            \
-)
-
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 };
 
 
 
-<<<<<<< HEAD
-=======
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
-      }
-    return MACRO_NONE;
-};
-
-
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 qk_tap_dance_action_t tap_dance_actions[] = {
    [DB_SQ_BRACK] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC,KC_RBRC),
    [DB_CR_BRACK] = ACTION_TAP_DANCE_DOUBLE(KC_LCBR, KC_RCBR),
    [DB_PR_BRACK] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN,KC_RPRN),
-<<<<<<< HEAD
-=======
-   [DB_QUOT]     = ACTION_TAP_DANCE_DOUBLE(KC_QUOT,KC_DQUO),
-   [DB_UNDS]     = ACTION_TAP_DANCE_DOUBLE(KC_MINS,KC_UNDS),
-   [DB_COLON]    = ACTION_TAP_DANCE_DOUBLE(KC_SCOLON,KC_COLN)
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
 };
 
 void matrix_init_user(void) {
@@ -358,7 +211,6 @@ void matrix_init_user(void) {
 }
 
 
-<<<<<<< HEAD
 
 bool is_alt_tab_active = false;
 uint16_t mnavig_timer;
@@ -416,13 +268,3 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 void matrix_scan_user(void) {
 
 }
-=======
-void matrix_scan_user(void) {
-
-}
-
-
-void led_set_user(uint8_t usb_led) {
-
-}
->>>>>>> 375aa9f6bc906835cd078bc0ca353471046cd36c
